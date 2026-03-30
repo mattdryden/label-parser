cb0293968dba4378d53ac7d25ce4f7c8cfe5cd17
mattdryden <48354684+mattdryden@users.noreply.github.com>
2026-03-30 22:18:52 +0100

    Add preview configuration to Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    preview: {
+    host: true,
+    port: process.env.PORT,
+    strictPort: true,
+    allowedHosts: [
+      'walrus-app-wj3f5.ondigitalocean.app'
+    ]
+  }
+,
   plugins: [
     react({
       babel: {
